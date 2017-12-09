00fd1530342ea73fe63fcab610e58491a7f2debb
ed85w <ed.walker@hotmail.co.uk>
2017-12-09 13:05:55 +0000

    added stock photos and tratment list

--- js/controller.js
+++ js/controller.js
@@ -4,4 +4,10 @@ angular.module('RouteControllers', [])
     .controller('TreatmentsController', function($scope) {
 
     	$scope.nails = nails;
+    	$scope.waxing = waxing;
+    	$scope.eyes = eyes;
+    	$scope.facials = facials;
+    	$scope.massage = massage;
+		$scope.threading = threading;
+
     });
--- js/treatments.js
+++ js/treatments.js
@@ -29,9 +29,107 @@ var nails = [
 
 // WAXING
 
+var waxing = [
+	{
+		description: 'Full Leg (45 mins)',
+		price: '£25'
+	},
+	{
+		description: 'Half Leg (30 mins)',
+		price: '£15'
+	},	
+	{
+		description: 'Bikini Line (30 mins)',
+		price: '£15'
+	},
+	{
+		description: 'Underarms (15 mins)',
+		price: '£10'
+	},
+	{
+		description: 'Eyebrow/Lips/Chin (15 mins)',
+		price: '£8'
+	},
+	{
+		description: 'Back (45 mins)',
+		price: '£20'
+	},
+	{
+		description: 'Chest (30 mins)',
+		price: '£20'
+	},
+]
+
 // EYES
+
+var eyes = [
+	{
+		description: 'Eyebrow Tint (15 mins)',
+		price: '£8'
+	},
+	{
+		description: 'Eyelash Tint (15 mins)',
+		price: '£10'
+	},	
+	{
+		description: 'Eyelash & Eyebrow Tint (30 mins)',
+		price: '£15'
+	},
+	{
+		description: 'Eyebrow Package: Tinting, Trimming, Waxing, Plucking, Threading (45 mins)',
+		price: '£28'
+	},
+]
 
 // FACIALS
 
+var facials = [
+	{
+		description: 'Express Facial (30 mins)',
+		price: '£35'
+	},
+	{
+		description: 'Essential Facial (60 mins)',
+		price: '£50'
+	},	
+]
+
 // MASSAGE
+
+var massage = [
+	{
+		description: 'Back, Neck & Shoulders (30 mins)',
+		price: '£35'
+	},
+	{
+		description: 'Back, Neck & Shoulders With Hot Stones (30 mins)',
+		price: '£45'
+	},	
+	{
+		description: 'Full Body (60 mins)',
+		price: '£55'
+	},
+		{
+		description: 'Full Body With Hot Stones (60 mins)',
+		price: '£70'
+	},
+]
+
+// THREADING 
+
+var threading = [
+	{
+		description: 'Eyebrow (30 mins)',
+		price: '£12'
+	},
+	{
+		description: 'Lip & Chin (15 mins)',
+		price: '£16'
+	},	
+	{
+		description: 'All of the above (45 mins)',
+		price: '£25'
+	},
+]
+
 
